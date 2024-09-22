539cfaa79dd47ede2ffde9371b5db1f595acb370
SatyamTindiyan <satyam23072000@gmail.com>
2024-09-22 20:19:06 +0530

    Add project files.

---
+++ WebApplication1/Controllers/TodoApiController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    [Consumes("application/json")]
+    public class TodoApiController : ControllerBase
+    {
+
+        private readonly TodoService _todoService;
+
+        public TodoApiController(TodoService todoService)
+        {
+            _todoService = todoService;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<TodoItem>> GetTodoItems()
+        {
+            return Ok(_todoService.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<TodoItem> GetTodoItem(long id)
+        {
+            var todoItem = _todoService.GetById(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(todoItem);
+        }
+
+        [HttpPost]
+        public ActionResult<TodoItem> PostTodoItem(TodoItem todoItem)
+        {
+            var newItem = _todoService.Add(todoItem);
+            return CreatedAtAction(nameof(GetTodoItem), new { id = newItem.Id }, newItem);
+        }
+
+        [HttpPut("{id}")]
+        
+        public IActionResult PutTodoItem(long id, TodoItem todoItem)
+        {
+            if (!_todoService.Update(id, todoItem))
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteTodoItem(long id)
+        {
+            if (!_todoService.Delete(id))
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+    }
+}
---
+++ WebApplication1/TodoService.cs
@@ -0,0 +1,43 @@
+﻿namespace WebApplication1
+{
+    public class TodoService
+    {
+        private readonly List<TodoItem> _todoItems = new List<TodoItem>();
+        private long _currentId = 1;
+
+        public IEnumerable<TodoItem> GetAll() => _todoItems;
+
+        public TodoItem GetById(long id)
+        {
+            return _todoItems.FirstOrDefault(x => x.Id == id);
+        }
+
+        public TodoItem Add(TodoItem newItem)
+        {
+            newItem.Id = _currentId++;
+            _todoItems.Add(newItem);
+            return newItem;
+        }
+
+        public bool Update(long id, TodoItem updatedItem)
+        {
+            var todoItem = _todoItems.FirstOrDefault(x => x.Id == id);
+            if (todoItem == null)
+                return false;
+
+            todoItem.Name = updatedItem.Name;
+            todoItem.IsComplete = updatedItem.IsComplete;
+            return true;
+        }
+
+        public bool Delete(long id)
+        {
+            var todoItem = _todoItems.FirstOrDefault(x => x.Id == id);
+            if (todoItem == null)
+                return false;
+
+            _todoItems.Remove(todoItem);
+            return true;
+        }
+    }
+}
